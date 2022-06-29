83806cffe9a6de615046dfedd7852c2a6bd4e3b8
Ethan <ethanchen2009@@hotmail.com>
2022-06-30 03:02:11 +0800

    Set debug to True

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECRET_KEY = os.environ.get("SECRET_KEY")
 SECRET_KEY = 'django-insecure-3pkm#2rmdm#gv730f7p6gl^o*923nd)34a8!$xt4xuwrsd(+-9'
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
