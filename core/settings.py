5e5745dba91b0a34263662001e33a579b064ae66
Ethan <email@email.com>
2022-06-30 01:26:30 +0800

    Reverted settings to develop phase

--- core/settings.py
+++ core/settings.py
@@ -22,8 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
-
+# SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = 'django-insecure-3pkm#2rmdm#gv730f7p6gl^o*923nd)34a8!$xt4xuwrsd(+-9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
